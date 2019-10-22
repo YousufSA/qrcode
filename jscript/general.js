c7d1b4886e3eb63de0580c473b515aebdeba9b3b
Yousuf S A <yousuf447@gmail.com>
2019-10-22 18:05:28 +0400

    qr brand image size validation

--- jscript/general.js
+++ jscript/general.js
@@ -1,4 +1,4 @@
 /* qr code generator
 Created by : Yousuf S A
 Date : 19th December 2018 */
-function generateQr(){var e=$("#data").val(),r=$("#brand").val(),a=$("#size").val(),d=$("#color").val(),t=a/4;""!=e?($("#qrcode").html(""),jQuery("#qrcode").qrcode({render:"canvas",text:e,width:a,height:a,background:"#ffffff",foreground:d,src:r,imgWidth:t,imgHeight:t})):alert("add enter data")}
+function generateQr(){var a=$("#data").val(),e=$("#brand").val(),r=$("#size").val(),d=$("#color").val(),n=$("#brandSize").val();""!=a?(n>r/3&&(n=r/3),$("#qrcode").html(""),jQuery("#qrcode").qrcode({render:"canvas",text:a,width:r,height:r,background:"#ffffff",foreground:d,src:e,imgWidth:n,imgHeight:n})):alert("add enter data")}$(document).ready(function(){$("#size").change(function(){var a=$("#size").val();a&&$("#brandSize").val(a/4)})});
